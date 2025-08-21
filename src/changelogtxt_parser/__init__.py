719f0c321ca22b4a1e2dd36891bc36da394b8943
David Angarita <davidangarita.sistemas@gmail.com>
2025-08-21 17:09:35 -0500

    Create load function for changelog parser

---
+++ src/changelogtxt_parser/__init__.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import pathlib
+import re
+from typing import TypedDict
+
+import semver
+from packaging import version
+
+VersionEntry = TypedDict("VersionEntry", {"version": str, "changes": list[str]})
+
+
+def _parse_version(content: str) -> semver.Version | version.Version | None:
+    content = content.lstrip("v")
+    v = None
+    try:
+        v = version.Version(content)
+    except version.InvalidVersion:
+        pass
+    try:
+        v = semver.Version.parse(content)
+    except ValueError:
+        pass
+    if not v:
+        return None
+    return v
+
+
+def _validate_path_file(path_file: str) -> pathlib.Path:
+    path = pathlib.Path(path_file)
+
+    if not path.is_absolute():
+        path = pathlib.Path(__file__).parent / path
+
+    if not path.exists():
+        raise FileNotFoundError(f"File not found: {path}")
+
+    return path
+
+
+def load(path_file: str) -> list[VersionEntry]:
+    file = _validate_path_file(path_file)
+
+    with file.open("r", encoding="utf-8") as f:
+        changelog: list[VersionEntry] = [{"version": "Unreleased", "changes": []}]
+        current = changelog[-1]
+
+        for raw in f:
+            line = raw.strip()
+            if not line:
+                continue
+
+            if _parse_version(line):
+                current: VersionEntry = {"version": line, "changes": []}
+                changelog.append(current)
+                continue
+
+            if re.match(r"^-", line):
+                current["changes"].append(line.lstrip("-").strip())
+                continue
+
+            current_list = current["changes"]
+            if current_list:
+                current_list[-1] += f" {line}"
+            else:
+                current_list.append(line)
+
+    return changelog
