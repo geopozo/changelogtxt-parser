746ae2bd1fde09017f2b31fb951362eb81d836ef
David Angarita <davidangarita.sistemas@gmail.com>
2025-08-22 12:00:39 -0500

    Add BadVersion to version file

--- src/changelogtxt_parser/version.py
+++ src/changelogtxt_parser/version.py
@@ -1,16 +1,89 @@
+import re
+from dataclasses import dataclass
+
 import semver
-from packaging import version
+from packaging import version as pyversion
 
 
-def parse_version(content: str) -> semver.Version | version.Version | None:
+# Class taken from:
+# https://github.com/geopozo/github-helper/blob/andrew/more_versions/src/github_helper/api/versions.py
+@dataclass(frozen=True, slots=True)
+class BadVersion:
+    """A weak parser that looks for instances where someone tried to tag."""
+
+    _tag_part_re = re.compile(r"^(\d*)(?:\.?(.*))?$")
+    tag: str
+    major: int
+    minor: int
+    micro: int
+    pre: None = None
+    local: str | None = None
+    is_prerelease: bool = False
+
+    def __init__(self, tag: str):
+        """Look for tag-like structures that parsers won't return."""
+        object.__setattr__(self, "tag", tag)
+        object.__setattr__(self, "major", 0)
+        object.__setattr__(self, "minor", 0)
+        object.__setattr__(self, "micro", 0)
+        object.__setattr__(self, "pre", None)
+        object.__setattr__(self, "local", None)
+        object.__setattr__(self, "is_prerelease", False)
+
+        tag = tag.removeprefix("v")
+
+        major_match = self._tag_part_re.search(tag)
+        if not major_match or not major_match.group(1):
+            raise ValueError
+
+        object.__setattr__(self, "major", int(major_match.group(1)))
+        if not major_match.group(2):
+            return
+
+        minor_match = self._tag_part_re.search(major_match.group(2))
+        if not minor_match or not minor_match.group(1):
+            object.__setattr__(self, "local", major_match.group(2))
+            return
+
+        object.__setattr__(self, "minor", int(minor_match.group(1)))
+        if not minor_match.group(2):
+            return
+
+        micro_match = self._tag_part_re.search(minor_match.group(2))
+        if not micro_match or not micro_match.group(1):
+            object.__setattr__(self, "local", minor_match.group(2))
+            return
+
+        object.__setattr__(self, "micro", int(micro_match.group(1)))
+        object.__setattr__(self, "local", micro_match.group(2) or None)
+
+    def __str__(self):
+        """Print Version as string."""
+        return self.__repr__()
+
+    def __repr__(self):
+        """Print Version as python-compatible string if possible."""
+        pre = f"{self.pre[0]}{self.pre[1]}" if self.pre is not None else ""
+        local = f"+{self.local}" if self.local is not None else ""
+        return f"{self.major}.{self.minor}.{self.micro}{pre}{local}"
+
+
+_VersionTypes = semver.Version | pyversion.Version | BadVersion
+
+
+def parse_version(content: str) -> _VersionTypes | None:
     content = content.removeprefix("v")
     v = None
     try:
-        v = version.Version(content)
-    except version.InvalidVersion:
+        v = pyversion.Version(content)
+    except pyversion.InvalidVersion:
         pass
     try:
         v = semver.Version.parse(content)
+    except ValueError:
+        pass
+    try:
+        v = BadVersion(content)
     except ValueError:
         pass
     if not v:
