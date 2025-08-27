8bd8b2a4d5a07bc3657d3ab002df9f2fec5df299
David Angarita <davidangarita.sistemas@gmail.com>
2025-08-27 15:30:50 -0500

    Add cli

---
+++ src/changelogtxt_parser/_cli.py
@@ -0,0 +1,50 @@
+import argparse
+import sys
+from typing import Any
+
+import changelogtxt_parser as changelog
+
+
+def _get_cli_args() -> tuple[argparse.ArgumentParser, dict[str, Any]]:
+    description = """changelogtxt helps you manage your changelog file.
+
+    See changelogtxt COMMAND --help for information about commands.
+    """
+
+    parser = argparse.ArgumentParser(
+        add_help=True,
+        conflict_handler="resolve",
+        description=description,
+    )
+
+    subparsers = parser.add_subparsers(dest="command")
+
+    check_tag = subparsers.add_parser(
+        "check-tag",
+        description="Verify that a tag in the changelog matches the provided tag.",
+        help="Checks if a tag in the changelog matches the specified tag.",
+    )
+    check_tag.add_argument(
+        "-t",
+        "--tag",
+        help="Tag name is required.",
+        required=True,
+    )
+
+    basic_args = parser.parse_args()
+    return parser, vars(basic_args)
+
+
+def run_cli() -> None:
+    parser, cli_args = _get_cli_args()
+    tag = cli_args.pop("tag", None)
+    command = cli_args.pop("command", None)
+
+    match command:
+        case "check-tag":
+            res = changelog.check_tag(tag)
+            sys.exit(int(not res))
+        case _:
+            print("No command supplied.", file=sys.stderr)
+            parser.print_help()
+            sys.exit(1)
