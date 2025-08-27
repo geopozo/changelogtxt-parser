0d8b8ecea80006f5947558023f9cd44e6cff1235
David Angarita <davidangarita.sistemas@gmail.com>
2025-08-27 16:27:45 -0500

    Add check-format command to cli

--- src/changelogtxt_parser/_cli.py
+++ src/changelogtxt_parser/_cli.py
@@ -31,6 +31,19 @@ def _get_cli_args() -> tuple[argparse.ArgumentParser, dict[str, Any]]:
         required=True,
     )
 
+    check_format = subparsers.add_parser(
+        "check-format",
+        description="Verify that changelog file has the correct format",
+        help="Check changelog format.",
+    )
+    check_format.add_argument(
+        "-f",
+        "--file",
+        help="Optional file path to check format.",
+        required=False,
+        default="./",
+    )
+
     basic_args = parser.parse_args()
     return parser, vars(basic_args)
 
@@ -38,11 +51,17 @@ def _get_cli_args() -> tuple[argparse.ArgumentParser, dict[str, Any]]:
 def run_cli() -> None:
     parser, cli_args = _get_cli_args()
     tag = cli_args.pop("tag", None)
+    file = cli_args.get("file", "")
     command = cli_args.pop("command", None)
-
     match command:
         case "check-tag":
             res = changelog.check_tag(tag)
+            sys.exit(int(not res))
+        case "check-format":
+            if not (path_file := changelog.find_changelogtxt_file(file)):
+                sys.exit(1)
+            print(f"File found in: {path_file}")
+            res = changelog.load(path_file)
             sys.exit(int(not res))
         case _:
             print("No command supplied.", file=sys.stderr)
