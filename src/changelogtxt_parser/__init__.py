5334d1dc8b54c22f457f9a80895b28deb128c54c
David Angarita <davidangarita.sistemas@gmail.com>
2025-09-02 16:02:03 -0500

    Export serdes in init file

--- src/changelogtxt_parser/__init__.py
+++ src/changelogtxt_parser/__init__.py
@@ -2,5 +2,12 @@
 """ChangelogTXT Parser Module."""
 
 from changelogtxt_parser.app import check_tag, compare_files, update
+from changelogtxt_parser.serdes import dump, load
 
-__all__ = ["check_tag", "compare_files", "update"]
+__all__ = [
+    "check_tag",
+    "compare_files",
+    "dump",
+    "load",
+    "update",
+]
