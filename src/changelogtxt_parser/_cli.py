3831802947f0ed03c8ffd4eda8ff39304044ac4d
David Angarita <davidangarita.sistemas@gmail.com>
2025-09-02 12:27:51 -0500

    Create update command for cli and import logger

--- src/changelogtxt_parser/_cli.py
+++ src/changelogtxt_parser/_cli.py
@@ -1,8 +1,12 @@
 import argparse
+import logging
 import sys
 from typing import Any
 
-from changelogtxt_parser import _utils, app, serdes
+from changelogtxt_parser import app, serdes
+
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
+logger = logging.getLogger(__name__)
 
 
 def _get_cli_args() -> tuple[argparse.ArgumentParser, dict[str, Any]]:
@@ -64,6 +68,30 @@ def _get_cli_args() -> tuple[argparse.ArgumentParser, dict[str, Any]]:
         "target",
         help="Second changelog file path.",
     )
+    update = subparsers.add_parser(
+        "update",
+        description="Add a new change message to the specified version.",
+        help="Creates a new version entry if it doesn't exist.",
+    )
+    update.add_argument(
+        "-t",
+        "--tag",
+        help="Tag name is required.",
+        required=True,
+    )
+    update.add_argument(
+        "-m",
+        "--message",
+        help="Message is required.",
+        required=True,
+    )
+    update.add_argument(
+        "-f",
+        "--file",
+        help="Optional file path.",
+        required=False,
+        default="./",
+    )
 
     basic_args = parser.parse_args()
     return parser, vars(basic_args)
@@ -75,20 +103,45 @@ def run_cli() -> None:
     file = cli_args.pop("file", "")
     source_file = cli_args.pop("source", "")
     target_file = cli_args.pop("target", "")
+    message = cli_args.pop("message", "")
     command = cli_args.pop("command", None)
 
     match command:
         case "check-tag":
-            res = app.check_tag(tag, file)
+            try:
+                app.check_tag(tag, file)
+                logger.info(f"Tag validation for {tag} was successful.")
+                reteval = 0
+            except (ValueError, TypeError):
+                logger.exception("Invalid tag")
+                reteval = 1
         case "check-format":
-            res, _ = serdes.load(file)
+            try:
+                serdes.load(file)
+                logger.info("Changelog format validation was successful.")
+                reteval = 0
+            except ValueError:
+                logger.exception("Invalid changelog format")
+                reteval = 1
         case "compare":
-            res = app.compare_files(source_file, target_file)
-            res = res or "No changes"
+            try:
+                res = app.compare_files(source_file, target_file)
+                logger.info(res)
+                reteval = 0
+            except ValueError:
+                logger.exception("No changes founded")
+                reteval = 1
+        case "update":
+            try:
+                app.update(tag, message, file)
+                logger.info("File update was successful and generated at: {file}")
+                reteval = 0
+            except ValueError:
+                logger.exception("File update failed")
+                reteval = 1
         case _:
-            res = "No command supplied."
+            logger.error("No command supplied.")
             parser.print_help()
             sys.exit(1)
 
-    _utils.logger.info(res)
-    sys.exit(int(not res))
+    sys.exit(reteval)
