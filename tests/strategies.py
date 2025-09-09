ddf8bf62f27601e00cbba22929ff172496c0ac9c
David Angarita <davidangarita.sistemas@gmail.com>
2025-09-09 11:00:53 -0500

    Move strategies to new file

---
+++ tests/strategies.py
@@ -0,0 +1,35 @@
+from hypothesis import strategies as st
+
+version_strings = st.builds(
+    lambda major, minor, patch, post, dev: f"v{major}.{minor}.{patch}{post}{dev}",
+    major=st.integers(min_value=0, max_value=99),
+    minor=st.integers(min_value=0, max_value=99),
+    patch=st.integers(min_value=0, max_value=99),
+    post=st.one_of(
+        st.just(""),
+        st.integers(min_value=0).map(lambda n: f".post{n}"),
+    ),
+    dev=st.one_of(
+        st.just(""),
+        st.integers(min_value=0).map(lambda n: f".dev{n}"),
+    ),
+)
+
+random_string = st.text(
+    alphabet=st.characters(whitelist_categories=("Ll", "Lu", "Nd")),
+    min_size=8,
+    max_size=16,
+)
+
+list_of_strings = st.lists(random_string, min_size=2, max_size=10, unique=True)
+
+list_of_version_entries = st.lists(
+    st.builds(
+        lambda version, change: {"version": version, "changes": [change]},
+        version=version_strings,
+        change=random_string,
+    ),
+    min_size=2,
+    max_size=10,
+    unique_by=lambda entry: str(entry["version"]),
+)
