f791e8eb2e40d01b41a8b704a5491908c954ba79
David Angarita <davidangarita.sistemas@gmail.com>
2025-09-09 11:23:23 -0500

    Fix serdes test

--- tests/test_serdes.py
+++ tests/test_serdes.py
@@ -1,16 +1,25 @@
-from changelogtxt_parser import serdes
-from changelogtxt_parser import version as version_tools
+from hypothesis import given, settings
 
-CHANGELOG_DATA: list[version_tools.VersionEntry] = [
-    {"version": "unreleased", "changes": ["New change"]},
-    {"version": "v1.0.0", "changes": ["First version"]},
-]
+from changelogtxt_parser import serdes
+from tests import strategies as sts
+
+BASE_SETTINGS = settings(max_examples=20)
+CHANGELOG_CONTENT = "v1.0.1\n- Fixed bug\n\nv1.0.0\n- Initial release"
 
 
 class TestRoundtrip:
-    def test_roundtrip(self, tmp_path):
-        file_path = tmp_path / "changelog.txt"
-        serdes.dump(CHANGELOG_DATA, file_path)
-        loaded = serdes.load(file_path)
+    @BASE_SETTINGS
+    @given(entries=sts.list_of_version_entries)
+    def test_roundtrip(
+        self,
+        entries,
+        tmp_path_factory,
+    ):
+        file = tmp_path_factory.mktemp("test") / "CHANGELOG.txt"
+        file.write_text(CHANGELOG_CONTENT)
+        entries.insert(0, {"version": "", "changes": []})
 
-        assert loaded == CHANGELOG_DATA
+        serdes.dump(entries, file)
+        loaded = serdes.load(file)
+
+        assert loaded == entries
